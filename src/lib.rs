119c6c9eb75bb2c3361c5ce33e9df20652590dd4
mart-mihkel <mart.mihkel.aun@gmail.com>
2023-10-14 20:30:04 +0300

    palette as command line argument

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use clap::Parser;
 use winit::dpi::PhysicalSize;
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
+use crate::palette::Palette;
 use crate::state::State;
 
 type ComplexNumber = [f32; 2];
@@ -15,10 +16,6 @@ type Rgb = [f32; 3];
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Frames per second limit
-    #[arg(long, default_value_t = 60f32)]
-    fps: f32,
-
     /// Julia parameter
     #[arg(long, value_parser = Self::parse_complex_number, default_value = "-0.162+1.04i")]
     constant: ComplexNumber,
@@ -30,6 +27,10 @@ pub struct Args {
     /// Maximum number of iterations per vertex when not using the shader
     #[arg(long, default_value_t = 250)]
     maximum_iterations: u32,
+
+    /// Color palette
+    #[arg(long, value_parser = Self::parse_palette, default_value = "ultra-fractal")]
+    palette: Palette,
 
     // todo resolution as command line argument
 }
@@ -51,6 +52,15 @@ impl Args {
         match s {
             "yes" => Ok(true),
             "no" => Ok(false),
+            _ => Err(MESSAGE),
+        }
+    }
+
+    fn parse_palette(s: &str) -> Result<Palette, &'static str> {
+        const MESSAGE: &str = "must be one of: ultra-fractal, green.";
+        match s {
+            "ultra-fractal" => Ok(Palette::UltraFractal),
+            "green" => Ok(Palette::Green),
             _ => Err(MESSAGE),
         }
     }
--- src/palette.rs
+++ src/palette.rs
@@ -1,6 +1,17 @@
 use crate::Rgb;
 
-const ULTRA_FRACTAL_PALETTE: [Rgb; 5] = [
+const GREEN: [Rgb; 8] = [
+    [0.0, 0.025, 0.0],
+    [0.0, 0.1, 0.0],
+    [0.0, 0.125, 0.0],
+    [0.0, 0.325, 0.0],
+    [0.0, 0.5, 0.0],
+    [0.0, 0.725, 0.0],
+    [0.0, 0.925, 0.0],
+    [0.0, 1.0, 0.0],
+];
+
+const ULTRA_FRACTAL: [Rgb; 5] = [
     [0.0, 0.027, 0.392],
     [0.124, 0.42, 0.796],
     [0.929, 1.0, 1.0],
@@ -8,8 +19,17 @@ const ULTRA_FRACTAL_PALETTE: [Rgb; 5] = [
     [0.0, 0.008, 0.0],
 ];
 
-pub fn pick(i: usize) -> Rgb {
-    ULTRA_FRACTAL_PALETTE[i % ULTRA_FRACTAL_PALETTE.len()]
+#[derive(Copy, Clone)]
+pub enum Palette {
+    Green,
+    UltraFractal,
+}
+
+pub fn pick(palette: Palette, i: usize) -> Rgb {
+    match palette {
+        Palette::Green => GREEN[i % GREEN.len()],
+        Palette::UltraFractal => ULTRA_FRACTAL[i % ULTRA_FRACTAL.len()],
+    }
 }
 
 pub fn linear_interpolate(start: Rgb, end: Rgb, weight: f32) -> Rgb {
