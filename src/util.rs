58a1174535567b669356b02f8e40522a00821e75
mart-mihkel <mart.mihkel.aun@gmail.com>
2023-10-18 20:53:14 +0300

    move julia_iter to shader

--- src/util.rs
+++ src/util.rs
@@ -1,31 +1,7 @@
-use std::ops::Neg;
 use wgpu::SurfaceError;
 use winit::event::{ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent};
 use winit::event_loop::ControlFlow;
-use crate::ComplexNumber;
 use crate::state::State;
-
-pub fn julia_iter(z: ComplexNumber, c: ComplexNumber, max_it: u32) -> (u32, f64) {
-    // todo use bigdecimal
-    // todo reduce number of multiplications via algebraic simplifications
-    let mut re = z[0];
-    let mut im = z[1];
-
-    let mut modulus = (re * re + im * im).sqrt();
-    let mut exp_smoothing = modulus.neg().exp();
-    let mut it = 0;
-    while it < max_it && modulus < 2.0 {
-        let temp = re;
-        re = temp * temp - im * im + c[0];
-        im = 2.0 * im * temp + c[1];
-
-        modulus = (re * re + im * im).sqrt();
-        exp_smoothing += modulus.neg().exp();
-        it += 1;
-    }
-
-    (it, exp_smoothing)
-}
 
 pub fn handle_event(mut state: &mut State, event: Event<()>, control_flow: &mut ControlFlow) {
     match event {
